48ee5b0f299173fc3eb895f47e21f4b9a3380c78
Angela <kafuigalley7@gmail.com>
2024-06-05 19:01:05 +0000

    feat: worked on ui and successfully connected to todo and finance app.

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,35 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+export default {
+  methods: {
+    goToFinanceTracker() {
+      window.location.href = 'https://finance-track-vue.vercel.app';
+    },
+    goToTodoList() {
+      window.location.href = 'https://dziku.vercel.app';
+    }
+  }
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<header>
+    <nav>
+      <ul>
+        <li>Vue Duo App</li>
+      </ul>
+    </nav>
+</header>
+
+  <div class="greeting">
+    <h1> Hello there! </h1>
+  </div>
+
+  <div class="enquiry"> 
+    <h3>What will you be doing today?</h3>
+  </div>
+
+  <div class="btn-click">
+    <button @click="goToFinanceTracker">Track Your Finances</button>
+    <button @click="goToTodoList">Manage Your Todo List</button>
+  </div>
 </template>
